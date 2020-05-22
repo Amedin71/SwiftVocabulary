b03f4fb31204e1df92e6d24e9d5a4b7199351e5c
Ashley Medina <ashley-medina@lambdastudents.com>
2020-05-21 19:51:17 -0600

    Not finished still working on it Ashley Medina

---
+++ Swift Vocabulary/DefinitionViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DefinitionViewController.swift
+//  Swift Vocabulary
+//
+//  Created by Ashley Medina on 5/21/20.
+//  Copyright © 2020 Ashley Medina. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var Word: UILabel!
+    
+    @IBOutlet weak var Definition: UITextView!
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Swift Vocabulary/Swift Vocabulary/VocabularyWord.swift
@@ -0,0 +1,9 @@
+//
+//  VocabularyWord.swift
+//  Swift Vocabulary
+//
+//  Created by Ashley Medina on 5/21/20.
+//  Copyright © 2020 Ashley Medina. All rights reserved.
+//
+
+import Foundation
